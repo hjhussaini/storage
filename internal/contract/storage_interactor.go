c0bbb784b9a499bf3069e7f9e0c97cdaadd767bc
Amir Hussain Haji Hussaini <hajihussaini@afrabon.com>
2023-02-07 16:34:14 +0330

    Refactoring code
    contract.StorageInteractor
    Add method Login()
    Add method Upload()

--- internal/contract/storage_interactor.go
+++ internal/contract/storage_interactor.go
@@ -1,4 +1,12 @@
 package contract
 
+import (
+    "context"
+
+    "github.com/hjhussaini/storage-srv-go/internal/dto"
+)
+
 type StorageInteractor interface {
+    Login(context.Context, *dto.LoginRequest) error
+    Upload(context.Context, *dto.UploadRequest) error
 }
