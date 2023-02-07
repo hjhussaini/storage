c4f7842337d579354bdd444c55e928f800c8af22
Amir Hussain Haji Hussaini <hajihussaini@afrabon.com>
2023-02-07 17:06:13 +0330

    v1.Handler
    Implement method upload()

--- internal/delivery/http/v1/upload.go
+++ internal/delivery/http/v1/upload.go
@@ -1,8 +1,24 @@
 package v1
 
 import (
+    "encoding/json"
     "net/http"
+
+    "github.com/hjhussaini/storage-srv-go/internal/dto"
 )
 
 func (handler *Handler) upload(response http.ResponseWriter, request *http.Request) {
+    var uploadRequest   dto.UploadRequest
+
+    response.Header().Set("Content-Type", "application/json")
+    err := json.NewDecoder(request.Body).Decode(&uploadRequest)
+    request.Body.Close()
+    if err != nil {
+        response.WriteHeader(http.StatusBadRequest)
+
+        return
+    }
+
+    handler.interactor.Upload(request.Context(), &uploadRequest)
+    response.WriteHeader(http.StatusOK)
 }
