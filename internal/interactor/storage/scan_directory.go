1aa874150110d20952cc076a002716439a1b1f31
Amir Hussain Haji Hussaini <hajihussaini@afrabon.com>
2023-02-12 13:43:38 +0330

    storage.Interactor
    Implement method ScanDirectory()

--- internal/interactor/storage/scan_directory.go
+++ internal/interactor/storage/scan_directory.go
@@ -2,9 +2,30 @@ package storage
 
 import (
     "context"
+    "log"
+    "os"
 
     "github.com/hjhussaini/storage-srv-go/internal/dto"
 )
 
 func (interactor *Interactor) ScanDirectory(ctx context.Context, request *dto.ScanRequest) {
+    entries, err := os.ReadDir(request.Directory)
+    if err != nil {
+        log.Println(err)
+
+        return
+    }
+
+    for _, entry := range entries {
+        if entry.IsDir() {
+            continue
+        }
+
+        uploadFileRequest := &dto.UploadRequest{
+            File:       request.Directory + "/" + entry.Name(),
+            To:         request.To + entry.Name(),
+            DeleteFile: request.DeleteFile,
+        }
+        go interactor.UploadFile(ctx, uploadFileRequest)
+    }
 }
